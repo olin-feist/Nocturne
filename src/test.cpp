1c797cc751ac77f6b281d3328f92aeda57703d9b
feisto <olin.feist@gmail.com>
2024-03-27 22:03:30 -0700

    build setup, done kinda

---
+++ src/test.cpp
@@ -0,0 +1,15 @@
+/*-------------------------------------------
+                Includes
+-------------------------------------------*/
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <iostream>
+
+#include "rknn_api.h"
+
+int main(){
+    std::cout<<"Hello World"<<std::endl;
+}
